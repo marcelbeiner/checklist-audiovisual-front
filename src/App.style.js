8f533fff6184c8e3ead50333baacc5cccde25143
Marcel Beiner <marcelbeiner@gmail.com>
2023-04-21 11:30:31 -0300

    iniciada a pagina principal

---
+++ src/App.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Page = styled.div`
+h1{
+    font-style: normal;
+    font-weight: 900;
+    font-size: 96px;
+    line-height: 131px;
+    text-align: center;
+    color: #FFF;
+    -webkit-text-stroke-width: 3px;
+    -webkit-text-stroke-color: #000;
+    span{
+        color: #90D3F9;
+    }
+}
+`
